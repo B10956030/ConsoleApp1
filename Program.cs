c177de6c8c77a7b32c337b6d4e9c72dbb34e83c3
B10956030 <84553747+B10956030@users.noreply.github.com>
2022-03-14 11:22:18 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int inputInt = int.Parse(intput);
+            
+            for (var i = 1; i <= inputInt; i++)
+            {
+                for (int j = 1; j <= 1; j++) 
+                {
+                    Console.Write(inputInt + "*" + i);
+                }
+            }
+            Console.Write();
+
+            for (var i = 1; i <= 9; i++) 
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + "*" + j + "=" + (i*j));
+                    Console.WriteLine( );
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
